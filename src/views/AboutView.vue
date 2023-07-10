99b6cd77c5e61a2af414dff6b89639f2198e2a22
salino3 <flaviobcn012@gmail.com>
2023-07-10 15:36:11 +0200

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,37 @@
+<script lang="ts" setup>
+import {computed, ref} from 'vue';
+
+const msg = ref('Hello Wolrd!');
+const message = "I'm the originalmessage";
+const myBool = ref(true);
+
+const reversedMessage = computed(() => {
+  return message.split('').reverse().join('');
+});
+
+const toggleText = () => {
+  myBool.value = !myBool.value;
+};
+</script>
+
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1><u>This is a code page</u></h1>
+    <h2>{{ msg }}</h2>
+    <input type="text" v-model="msg">
+    <br>
+    <p class="click-p" @click="toggleText">{{myBool ? message : reversedMessage }}</p>
   </div>
 </template>
+
+
+<style>
+
+.click-p {
+  cursor: pointer;
+}
+</style>
+
+
+
